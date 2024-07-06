25caa1d050ec56828949a2cb88ecc1d2e649e133
Nour Elnbawy <147569563+Elnbawy@users.noreply.github.com>
2024-07-06 20:04:15 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'creamy':'#E1CDB5',
+        'uViolet':'#49306B'
+      }
+    },screens: {
+    'xs':'450px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [],
+}
+
